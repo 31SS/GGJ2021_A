7a001d2b574a961d91d6dc1a49338d003f80f592
tmshr <58304948+tmshr@users.noreply.github.com>
2021-01-30 04:03:01 +0900

    ポーズメニュー、時間制限追加

---
+++ Assets/takano/Script/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //escキーでポーズメニューを非アクティブ
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+
+        }
+    }
+    //続けるボタン
+    public void ClickReturn()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    //はじめからボタン
+    public void ClickRetry()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene("mainScene");
+        Time.timeScale = 1f;
+    }
+}
