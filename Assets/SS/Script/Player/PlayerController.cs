16a75960ee5e75114341df58c0ce75ea4cfff8f6
DESKTOP-89F0FVT\SS <sato991105@outlook.jp>
2021-01-30 13:10:05 +0900

    完全体Player 欠損モーション　実装中

--- Assets/SS/Script/Player/PlayerController.cs
+++ Assets/SS/Script/Player/PlayerController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using BodyNumber;
 using CharacterState;
 using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
 
 public class PlayerController : MonoBehaviour
 {
     //変更前のステート名
-    protected string _prevStateName;
+    private string _prevStateName;
     
     //ステート
     public StateProcessor StateProcessor { get; set; } = new StateProcessor();
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour
     public CharacterStateAir StateAir { get; set; } = new CharacterStateAir();
     public CharacterStateOver StateOver { get; set; } = new CharacterStateOver();
 
+    public List<GameObject> bodyParts;
+    
     [SerializeField] private PlayerParameter playerParameter;
     
     [SerializeField] protected Rigidbody2D m_rigidbody2D;
@@ -24,6 +28,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] protected bool m_isGround/* { get; set; }*/;
     [SerializeField] protected ContactFilter2D _groundFilter2D;
 
+    private bool moveableFlag;
+    private bool  highMoveableFlag;
+    private bool  openableDoorFlag;
+    private bool  viewableFieldFlag;
+    private bool highViewableFieldFlag;
+    private bool  hearableFlag;
     private void Awake()
     {
         StateProcessor.State.Value = StateIdle;
@@ -34,6 +44,7 @@ public class PlayerController : MonoBehaviour
     }
     private void Start()
     {
+        // var jumpableFlag = false;
         //ステートの値が変更されたら実行処理を行うようにする
         StateProcessor.State
             .Where(_ => StateProcessor.State.Value.GetStateName() != _prevStateName)
@@ -44,6 +55,16 @@ public class PlayerController : MonoBehaviour
                 StateProcessor.Execute();
             })
             .AddTo(this);
+        
+        inisActionFlag();
+        // this.UpdateAsObservable()
+        //     .Where(_ => (bodyParts[Define.RIGHTLEG].activeSelf || bodyParts[Define.LEFTLEG].activeSelf))
+        //     .Subscribe(_ => jumpableFlag = true);
+
+        foreach (var part in bodyParts)
+        {
+            part.SetActive(false);
+        }
     }
 
     private void Update()
@@ -54,30 +75,90 @@ public class PlayerController : MonoBehaviour
         // m_animator.SetFloat("Vertical", m_rigidbody2D.velocity.y);
         // m_animator.SetBool("isGround", m_isGround);
         m_isGround = m_rigidbody2D.IsTouching(_groundFilter2D);
-        m_rigidbody2D.velocity = new Vector2(_x * playerParameter.RUN_SPEED, m_rigidbody2D.velocity.y);
 
         // _playerMover.Move(NON_REVERSE, playerParameter.RUN_SPEED, _playerInput.X, m_isGround, m_animator);
-        if (m_isGround)
+        if (moveableFlag || highMoveableFlag)
         {
-            if (_jump)
+            var _velosity = m_rigidbody2D.velocity;
+
+            m_rigidbody2D.velocity = highMoveableFlag
+                ? new Vector2(_x * playerParameter.HIGHRUN_SPEED, _velosity.y)
+                : new Vector2(_x * playerParameter.RUN_SPEED, _velosity.y);
+            if (m_isGround)
             {
-                // m_animator.SetTrigger("Jump");
-                m_rigidbody2D.AddForce(Vector2.up * playerParameter.JUMP_POWER);
-                StateProcessor.State.Value = StateAir;
-            }
-            else if (Mathf.Abs(_x) > 0)
-            {
-                StateProcessor.State.Value = StateRun;
-                var rot = transform.rotation;
-                // transform.rotation = Quaternion.Euler(rot.x, Mathf.Sign(_playerInput.X) == 1 ? 0 : 180, rot.z);
-            }
-            else
-            {
-                StateProcessor.State.Value = StateIdle;
+                if (_jump)
+                {
+                    // m_animator.SetTrigger("Jump");
+                    if (highMoveableFlag)
+                    {
+                        m_rigidbody2D.AddForce(Vector2.up * playerParameter.HIGHJUMP_POWER);
+                    }
+                    else
+                    {
+                        m_rigidbody2D.AddForce(Vector2.up * playerParameter.JUMP_POWER);
+                    }
+
+                    StateProcessor.State.Value = StateAir;
+                }
+
+                else if (Mathf.Abs(_x) > 0)
+                {
+                    StateProcessor.State.Value = StateRun;
+                    var rot = transform.rotation;
+                    // transform.rotation = Quaternion.Euler(rot.x, Mathf.Sign(_playerInput.X) == 1 ? 0 : 180, rot.z);
+                }
+                else
+                {
+                    StateProcessor.State.Value = StateIdle;
+                }
             }
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var pickupable = other.GetComponent<IPickupable>();
+        if (pickupable != null)
+        {
+            pickupable.PickedUp(this);
+            Debug.Log("Caught");
+        }
+    }
+
+    private void inisActionFlag()
+    {
+        moveableFlag = false;
+        highMoveableFlag = false;
+        openableDoorFlag = false;
+        viewableFieldFlag = false;
+        highViewableFieldFlag = false;
+        hearableFlag = false;
+
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.RIGHTLEG].activeSelf || bodyParts[Define.LEFTLEG].activeSelf))
+            .Subscribe(_ => moveableFlag = true);
+        
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.RIGHTLEG].activeSelf && bodyParts[Define.LEFTLEG].activeSelf))
+            .Subscribe(_ => highMoveableFlag = true);
+        
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.RIGHTHAND].activeSelf || bodyParts[Define.LEFTHAND].activeSelf))
+            .Subscribe(_ => openableDoorFlag = true);
+        
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.RIGHTEYE].activeSelf || bodyParts[Define.LEFTEYE].activeSelf))
+            .Subscribe(_ => viewableFieldFlag = true);
+        
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.RIGHTEYE].activeSelf && bodyParts[Define.LEFTEYE].activeSelf))
+            .Subscribe(_ => highViewableFieldFlag = true);
+        
+        this.UpdateAsObservable()
+            .Where(_ => (bodyParts[Define.HEAD].activeSelf))
+            .Subscribe(_ => hearableFlag = true);
+    }
+    
     public void Idle()
     {
 
