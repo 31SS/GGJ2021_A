653e8c43b11b2e59a79a20cb548d4a3843a6cb57
DESKTOP-89F0FVT\SS <sato991105@outlook.jp>
2021-01-31 15:20:30 +0900

    Title シーン遷移　実装

---
+++ Assets/SS/Script/SceneTransition/SceneMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class SceneMover : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("aaa");
+    }
+ 
+}
