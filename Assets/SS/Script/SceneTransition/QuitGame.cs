f48c915cce7d4aa1c2f492c054b456fb2b6d3e8e
tmshr <58304948+tmshr@users.noreply.github.com>
2021-01-31 16:53:58 +0900

    最新

--- Assets/SS/Script/SceneTransition/QuitGame.cs
+++ Assets/SS/Script/SceneTransition/QuitGame.cs
@@ -6,10 +6,6 @@ public class QuitGame : MonoBehaviour
 {
     public void ButtonClicked()
     {
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;   // UnityEditorの実行を停止する処理
-#else
         Application.Quit();                                // ゲームを終了する処理
-#endif
     }
 }
